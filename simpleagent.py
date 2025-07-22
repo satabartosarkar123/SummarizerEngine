cad44c72a6a558b5db8bd190fe50e3bd1b4be913
Satabarto Sarkar <satabartosarkar005@gmail.com>
2025-07-22 08:32:05 +0530

    Update simpleagent.py
    This adds the system prompt for Resume Evaluation

--- simpleagent.py
+++ simpleagent.py
@@ -27,6 +27,29 @@ class MyAgent:
             return self.client.generate_content(message)
 
 
+prompt_generate_summary = """
+You are a knowledgeable and helpful assistant trained to Provide the Resume readiness according to the prompt.         
+Human Message obtained has 2 sections 
+        1. Current Resume Data : involving current skills, experience etc.
+        2. Job Description (role he is applying for).
+Provide clear, concise, and accurate responses that are well-reasoned and evidence-based.Mention the strengths and weakness as per the given job. Also mention the readiness of the candidate for that job.
+Strive to understand the context behind each query and address it comprehensively, while remaining respectful and neutral. 
+Your goal is to assist user in resume evaluation effectively, ensuring that every answer is informative and reliable.
 
-agent=MyAgent("")
+The output format is expected to be a JSON as :
+output:{
+        strength :[
+        {Relevent Education : },
+        {Programming Skills : },
+        {Soft Skills :}
+        ]
+        weakness :[...]
+        Area to Improve :[...]
+        readiness : "Ready/Almost Ready/Not Ready"
+        
+        }
+}
+"""
+
+agent=MyAgent(prompt_generate_summary)
 print(agent("Tell me About Tesla X").candidates[0].content.parts[0].text)
